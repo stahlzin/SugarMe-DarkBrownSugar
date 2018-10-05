a75b01c934d6d8f28f686b059f768cb2f5b0419e
stahlzin <stahlzin@users.noreply.github.com>
2018-10-05 16:39:08 -0300

    Alterações no Layout de Entrada
    Falta consertar a imagem do Logo. 100% Funcional

--- app/src/main/java/br/com/mateus/sugarme/View/MainActivity.java
+++ app/src/main/java/br/com/mateus/sugarme/View/MainActivity.java
@@ -3,6 +3,7 @@ package br.com.mateus.sugarme.View;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import br.com.mateus.sugarme.R;
 
@@ -15,12 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mainController = new MainController();
 
+        //Maximixar a tela
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
        if(mainController.verificaLogin()){
            mainController.verificaTipoUsuario(MainActivity.this);
        }
        else{
            mainController.fazerLogin(this);
        }
+
+
     }
 
     @Override
