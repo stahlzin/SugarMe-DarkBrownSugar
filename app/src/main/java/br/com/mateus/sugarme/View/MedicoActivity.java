3779ed36c538e0dc05961c4bf292560c983648ed
stahlzin <stahlzin@users.noreply.github.com>
2018-11-06 11:27:25 -0200

    Interface do Médico Implementada
    Menus e sub-menus criados

--- app/src/main/java/br/com/mateus/sugarme/View/MedicoActivity.java
+++ app/src/main/java/br/com/mateus/sugarme/View/MedicoActivity.java
@@ -5,18 +5,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 
 import br.com.mateus.sugarme.Controller.MedicoPresenter;
 import br.com.mateus.sugarme.Model.Medico;
 import br.com.mateus.sugarme.R;
+
+import static br.com.mateus.sugarme.Factory.NavigationFactory.FinishNavigation;
+import static br.com.mateus.sugarme.Factory.NavigationFactory.SimpleNavigation;
 
 public class MedicoActivity extends AppCompatActivity {
     private MedicoPresenter medicoPresenter;
     private Button novaEntradaButton;
     private Button buttonEditarMedico;
     private Medico medico;
+    private GridLayout perfilMedicoGridLayout;
+    private GridLayout chatMedicoGridLayout;
+    private GridLayout pacientesMedicoGridLayout;
+    private GridLayout medicoConfiguracoesGridLayout;
 
 
     @Override
@@ -30,18 +39,41 @@ public class MedicoActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(R.string.app_name);
         medicoPresenter = new MedicoPresenter();
 
-        novaEntradaButton = (Button) findViewById(R.id.novaEntradaButton);
+        perfilMedicoGridLayout = (GridLayout) findViewById(R.id.perfilMedicoGridLayout);
+        chatMedicoGridLayout = (GridLayout) findViewById(R.id.chatMedicoGridLayout);
+        pacientesMedicoGridLayout = (GridLayout) findViewById(R.id.pacientesMedicoGridLayout);
+        medicoConfiguracoesGridLayout = (GridLayout) findViewById(R.id.medicoConfiguracoesGridLayout);
 
-        //Logout Medico
-        novaEntradaButton.setOnClickListener(new View.OnClickListener() {
+        //Configuração do Menu em GridLayout
+        perfilMedicoGridLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                medicoPresenter.logout();
-                Intent intent = new Intent(MedicoActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                MedicoActivity.this.startActivity(intent);
+                medicoPresenter.recebeMedico(MedicoActivity.this);
             }
         });
+
+        chatMedicoGridLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Chamar o chat
+            }
+        });
+
+        pacientesMedicoGridLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Listar Pacientes, relatorio
+            }
+        });
+
+        medicoConfiguracoesGridLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Configurações
+            }
+        });
+        //Fim da configuração do Menu
+
 
     }
 
@@ -51,5 +83,30 @@ public class MedicoActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.medico, menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if (id == R.id.action_about){
+            return true;
+        }
+        else if (id == R.id.action_sair){
+            medicoPresenter.logout();
+            FinishNavigation(MedicoActivity.this, MainActivity.class);
+            return true;
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
