da9b6a57789a2be4fbf7dd60c4f4e348cb3aac02
stahlzin <stahlzin@users.noreply.github.com>
2018-11-21 16:29:24 -0200

    NavFact corrigida

--- app/src/main/java/br/com/mateus/sugarme/Factory/NavigationFactory.java
+++ app/src/main/java/br/com/mateus/sugarme/Factory/NavigationFactory.java
@@ -13,7 +13,6 @@ public class NavigationFactory  {
 
     public static void SimpleNavigation (Activity atual, Class proxima) {
         Intent intent = new Intent(atual, proxima);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         atual.startActivity(intent);
     }
 
